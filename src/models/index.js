acbe786a1043ff5acf3055eb4be2f4c6c8a8ff6d
Jonathan Brooks <jonbrooks0122@gmail.com>
2023-07-12 21:00:04 -0700

    started collection (lab04)

--- src/models/index.js
+++ src/models/index.js
@@ -3,6 +3,7 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const movies = require('./movies.model');
 const games = require('./games.model.js');
+const Collection = require('./collection.js');
 
 const POSTGRES_URI = process.env.NODE_ENV === 'test' ? 'sqlite:memory' : process.env.DATABASE_URI;
 
@@ -10,10 +11,27 @@ let sequelize = new Sequelize(POSTGRES_URI, {
   dialect: 'postgres',
 });
 
+const movieModel = movies(sequelize, DataTypes);
+const gameModel = games(sequelize, DataTypes);
+
+movieModel.hasMany(gameModel, {
+  foreignKey: 'customerId',
+  sourceKey: 'id',
+});
+gameModel.belongsTo(movieModel, {
+  foreignKey: 'customerId',
+  targetKey: 'id',
+});
+
+const movieCollection = new Collection(movieModel);
+const gameCollection = new Collection(gameModel);
+
 module.exports = {
   dbConnection: sequelize,
   Movies: movies(sequelize, DataTypes),
-  Games: games(sequelize, DataTypes)
+  Games: games(sequelize, DataTypes),
+  movieCollection,
+  gameCollection,
 };
 
 // you might have to have a different data model since we have to create an association between two of our models (one to one/one to many/etc.)
